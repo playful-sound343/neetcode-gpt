58f5bdeeac9c3b4f2cadd5d24a16b674669a4f9d
playful-sound343 <ruturajajoshi2006@gmail.com>
2026-08-31 18:08:26 +0530

    Implement Activation functions (Math Foundations 2/6)

---
+++ foundations/activations.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        output=1/(1+np.exp(-z))
+        return np.round(output,5)
+        pass
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        output=np.maximum(0,z)
+        return np.round(output,5)
+        pass
